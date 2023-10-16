2f1b960a112a88bc5490ef863ddf67e9d1313b55
Jakub Hajduk (PGS Software) <jhajduk@pgs-soft.com>
2023-10-16 16:39:01 +0200

    ggg

---
+++ build.mjs
@@ -0,0 +1,20 @@
+import { readFileSync, writeFileSync } from 'fs'
+
+const pkg = JSON.parse( readFileSync('./package.json') )
+
+const template = (title = 'Title', body = 'elo') => `<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>${title}</title>
+</head>
+<body>
+    <h1>${title}</h1>
+    <p>${body}</p>
+</body>
+</html>`;
+
+writeFileSync('./dist/index.html', template(`Version ${ pkg.version }`, `${ pkg.name } by ${ pkg.author } in ${ pkg.version }. Cheers!`), 'utf-8')
