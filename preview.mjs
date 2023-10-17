f44173cd25464d6721e29a16f7c4e373b5804abf
Jakub Hajduk (PGS Software) <jhajduk@pgs-soft.com>
2023-10-17 16:59:17 +0200

    ii

---
+++ preview.mjs
@@ -0,0 +1,25 @@
+import { readFileSync, writeFileSync, mkdirSync } from 'fs'
+
+const pkg = JSON.parse( readFileSync('./package.json') )
+
+const template = (title = 'Title', body = 'elo') => `<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>${title}</title>
+</head>
+<body>
+    <h1>
+        ${title}
+    </h1>
+    <p>
+        ${body}
+    </p>
+</body>
+</html>`;
+
+mkdirSync('./preview')
+writeFileSync('./preview/index.html', template(`Components preview ${ pkg.version }.`, `id: ${Math.floor(Math.random() * 1000)}\n Preview build in ${ pkg.name } by ${ pkg.author } in ${ pkg.version }. Cheers!`), 'utf-8')
